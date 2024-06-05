761a0d7e610873afb803553fb2c9e7b7999583a5
Max Haase <maxhaase@gmail.com>
2024-06-05 23:47:40 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,44 +1,26 @@
 #!/bin/bash
 
-# Start Apache
-service apache2 start
-
-# Start MariaDB
+# Start MySQL service
 service mysql start
 
-# Secure MariaDB installation
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User = 'root'"
-mysql -e "DELETE FROM mysql.user WHERE User=''"
-mysql -e "DROP DATABASE test"
-mysql -e "FLUSH PRIVILEGES"
+# Wait for MySQL to be ready
+while ! mysqladmin ping --silent; do
+    sleep 1
+done
 
-# Create databases and users
-mysql -e "CREATE DATABASE ${MYSQL_DATABASE}"
-mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%'"
-mysql -e "FLUSH PRIVILEGES"
+# Initialize MySQL database and users
+mysql -u root -p${MYSQL_ROOT_PASSWORD} <<EOF
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
 
-# Enable Apache sites
-a2ensite mail.${DOMAIN1}.conf
-a2ensite admin.${DOMAIN1}.conf
-a2ensite ${DOMAIN1}.conf
-if [ -n "$DOMAIN2" ]; then
-  a2ensite ${DOMAIN2}.conf
+# Run Apache configuration script
+/usr/local/bin/apache-config.sh
+
+# Set up Certbot for SSL certificates
+certbot --apache --non-interactive --agree-tos --email admin@${DOMAIN1} -d ${DOMAIN1} -d ${MAIL_DOMAIN} -d ${ADMIN_DOMAIN} -d ${WEBMAIL_DOMAIN}
+if [ ! -z "${DOMAIN2}" ]; then
+    certbot --apache --non-interactive --agree-tos --email admin@${DOMAIN1} -d ${DOMAIN2}
 fi
-a2ensite webmail.${DOMAIN1}.conf
-a2enmod proxy proxy_http ssl
-
-# Reload Apache
-service apache2 reload
-
-# Obtain SSL certificates
-certbot --apache -d mail.${DOMAIN1} -d admin.${DOMAIN1} -d ${DOMAIN1} -d ${DOMAIN2} -d webmail.${DOMAIN1} --non-interactive --agree-tos -m admin@${DOMAIN1}
-
-# Start Postfix
-service postfix start
-
-# Start Dovecot
-service dovecot start
-
-# Keep the container running
-tail -f /dev/null
