9449290d686460b15536d90a72ae24cc1338b56f
Max Haase <maxhaase@gmail.com>
2024-06-05 18:36:05 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-# Start MySQL service
+# Start services
 service mysql start
-
-# Configure MySQL
-mysql -u root -p$MYSQL_ROOT_PASSWORD <<EOF
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
-
-# Enable Apache modules and sites
-a2enmod proxy proxy_http ssl
-a2ensite admin.${DOMAIN1}.conf webmail.${DOMAIN1}.conf ${DOMAIN1}.conf ${DOMAIN2}.conf
-a2enconf roundcube postfixadmin
-
-# Enable and start services
 service apache2 start
 service postfix start
 service dovecot start
 service supervisor start
 
-# Obtain SSL certificates
-certbot --apache -d ${DOMAIN1} -d ${DOMAIN2} -d admin.${DOMAIN1} -d webmail.${DOMAIN1}
+# Configure MySQL
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# Enable Apache sites
+a2ensite ${MAIL_DOMAIN}.conf
+a2ensite ${ADMIN_DOMAIN}.conf
+a2ensite ${DOMAIN1}.conf
+a2ensite ${DOMAIN2}.conf
+a2ensite ${WEBMAIL_DOMAIN}.conf
+
+# Get SSL certificates
+certbot --apache --non-interactive --agree-tos --email admin@${DOMAIN1} -d ${DOMAIN1} -d ${DOMAIN2} -d ${MAIL_DOMAIN} -d ${ADMIN_DOMAIN} -d ${WEBMAIL_DOMAIN}
+
+# Restart Apache to apply changes
+service apache2 restart
 
 # Keep container running
 tail -f /var/log/apache2/access.log
