7001dd75f5f8ea9b33741164c017e77581e66d64
Max Haase <maxhaase@gmail.com>
2024-06-05 19:06:24 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
-# Start services
-service mysql start
+# Start Apache
 service apache2 start
-service postfix start
-service dovecot start
-service supervisor start
 
-# Configure MySQL
-mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
+# Start MariaDB
+service mysql start
+
+# Secure MariaDB installation
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User = 'root'"
+mysql -e "DELETE FROM mysql.user WHERE User=''"
+mysql -e "DROP DATABASE test"
+mysql -e "FLUSH PRIVILEGES"
+
+# Create databases and users
+mysql -e "CREATE DATABASE ${MYSQL_DATABASE}"
+mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%'"
+mysql -e "FLUSH PRIVILEGES"
 
 # Enable Apache sites
-a2ensite ${MAIL_DOMAIN}.conf
-a2ensite ${ADMIN_DOMAIN}.conf
+a2ensite mail.${DOMAIN1}.conf
+a2ensite admin.${DOMAIN1}.conf
 a2ensite ${DOMAIN1}.conf
-a2ensite ${DOMAIN2}.conf
-a2ensite ${WEBMAIL_DOMAIN}.conf
+if [ -n "$DOMAIN2" ]; then
+  a2ensite ${DOMAIN2}.conf
+fi
+a2ensite webmail.${DOMAIN1}.conf
+a2enmod proxy proxy_http ssl
 
-# Get SSL certificates
-certbot --apache --non-interactive --agree-tos --email admin@${DOMAIN1} -d ${DOMAIN1} -d ${DOMAIN2} -d ${MAIL_DOMAIN} -d ${ADMIN_DOMAIN} -d ${WEBMAIL_DOMAIN}
+# Reload Apache
+service apache2 reload
 
-# Restart Apache to apply changes
-service apache2 restart
+# Obtain SSL certificates
+certbot --apache -d mail.${DOMAIN1} -d admin.${DOMAIN1} -d ${DOMAIN1} -d ${DOMAIN2} -d webmail.${DOMAIN1} --non-interactive --agree-tos -m admin@${DOMAIN1}
 
-# Keep container running
-tail -f /var/log/apache2/access.log
+# Start Postfix
+service postfix start
+
+# Start Dovecot
+service dovecot start
+
+# Keep the container running
+tail -f /dev/null
