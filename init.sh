729362eab8b0f90ac551b495e3d7f2d60ab5647b
Max Haase <maxhaase@gmail.com>
2024-06-06 01:03:00 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
-# Start MySQL service
-service mysql start
+service mariadb start
 
-# Wait for MySQL to be ready
-while ! mysqladmin ping --silent; do
-    sleep 1
-done
+# Secure MariaDB installation
+mysql -u root <<-EOSQL
+  UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User='root';
+  DELETE FROM mysql.user WHERE User='';
+  DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+  FLUSH PRIVILEGES;
+EOSQL
 
-# Initialize MySQL database and users
-mysql -u root -p${MYSQL_ROOT_PASSWORD} <<EOF
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
+mysql -u root -p${MYSQL_ROOT_PASSWORD} <<-EOSQL
+  CREATE DATABASE ${MYSQL_DATABASE};
+  CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+  GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+  FLUSH PRIVILEGES;
+EOSQL
 
-# Run Apache configuration script
-/usr/local/bin/apache-config.sh
+# Enable Apache sites and modules
+a2enmod proxy proxy_http ssl
+a2ensite mail.DOMAIN1.conf
+a2ensite admin.DOMAIN1.conf
+a2ensite DOMAIN1.conf
+a2ensite DOMAIN2.conf
+a2ensite webmail.DOMAIN1.conf
 
-# Set up Certbot for SSL certificates
-certbot --apache --non-interactive --agree-tos --email admin@${DOMAIN1} -d ${DOMAIN1} -d ${MAIL_DOMAIN} -d ${ADMIN_DOMAIN} -d ${WEBMAIL_DOMAIN}
-if [ ! -z "${DOMAIN2}" ]; then
-    certbot --apache --non-interactive --agree-tos --email admin@${DOMAIN1} -d ${DOMAIN2}
-fi
+service apache2 restart
+
+# Configure postfixadmin and roundcube
+dpkg-reconfigure postfixadmin
+dpkg-reconfigure roundcube-core
+
+service postfix start
+service dovecot start
+
+# Setup Certbot
+certbot --apache --agree-tos --non-interactive --email admin@lung.se -d mail.DOMAIN1 -d admin.DOMAIN1 -d DOMAIN1 -d DOMAIN2 -d webmail.DOMAIN1
+
+# Supervisor
+/usr/bin/supervisord -n
